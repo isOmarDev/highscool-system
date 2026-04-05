a9d8192bf93dc7c439fc8e7a8701a3d6dd352be4
omar kamel <isomardev@gmail.com>
2026-04-05 21:00:04 +0200

    test: reset database before running tests

--- characterization-testing/tests/setup.ts
+++ characterization-testing/tests/setup.ts
@@ -1,4 +1,10 @@
 import { prisma } from '../src/database';
+import { resetDatabase } from './fixtures';
+
+beforeAll(async () => {
+  // Reset database before running tests
+  await resetDatabase();
+});
 
 afterAll(async () => {
   // Disconnect Prisma so Jest can exit cleanly
